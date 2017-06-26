b807f6a475c6946da49c1d0e252696528c34f88f
Tinh Thach Hinh <htthach@yahoo.com>
2017-06-26 21:43:07 +0800

    dev tool initial commit

---
+++ lib/thach_view_tool.rb
@@ -0,0 +1,6 @@
+require "thach_view_tool/version"
+require "thach_view_tool/renderer"
+
+module ThachViewTool
+  # Your code goes here...
+end
---
+++ lib/thach_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ThachViewTool
+  VERSION = "0.1.0"
+end
